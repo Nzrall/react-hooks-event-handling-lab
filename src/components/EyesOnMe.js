6b10802fa8c6b1541da75963e7444df5d637dd02
Joshua <wilson.joshua@outlook.com>
2021-11-08 16:04:28 -0800

    commit tree fa05ccbaa3312da9076d54ad683e88c27c8b4039

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,34 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+
+// function focus () {
+//     return (
+//         console.log('Good!')
+//     );
+// }
+// function blur () {
+//     return (
+//         console.log('Hey! Eyes On Me!')
+//     );
+// }
+
+function EyesOnMe () {
+    function focus () {
+        return (
+            console.log('Good!')
+        );
+    }
+    function blur () {
+        return (
+            console.log('Hey! Eyes On Me!')
+        );
+    }
+return (
+    <div> 
+        <button onFocus={focus} onBlur={blur}>Eyes on Me</button>
+    </div>
+    );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,18 @@
 // Code Keypad Component Here
+import React from 'react';
+
+function passwordSubmit() {
+    return (
+        console.log('Entering password...')
+    );
+}
+
+function Keypad () {
+    return (
+    <div>
+        <input type="password" onChange={passwordSubmit} ></input>
+    </div>
+    )
+}
+
+export default Keypad;
